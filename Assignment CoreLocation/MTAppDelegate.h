fdbd4b7a32028613a37e2775b8796d62acdfc026
Michael Tirenin <michaeltirenin@Michaels-MacBook-Pro-2.local>
2014-05-21 11:05:42 -0700

    Initial Commit

---
+++ Assignment CoreLocation/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  Assignment CoreLocation
+//
+//  Created by Michael Tirenin on 5/21/14.
+//  Copyright (c) 2014 Michael Tirenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignment CoreLocationTests/Assignment_CoreLocationTests.m
@@ -0,0 +1,34 @@
+//
+//  Assignment_CoreLocationTests.m
+//  Assignment CoreLocationTests
+//
+//  Created by Michael Tirenin on 5/21/14.
+//  Copyright (c) 2014 Michael Tirenin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Assignment_CoreLocationTests : XCTestCase
+
+@end
+
+@implementation Assignment_CoreLocationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
